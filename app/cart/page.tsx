1cc9758d7fda8546682b5f4d2205ee4ad1a4a2f2
Sakshi <gabajisakshi@gamil.com>
2026-06-21 17:53:27 +0530

    Add bcryptjs dependency

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -13,7 +13,7 @@ export default function CartPage() {
   const { cart, increaseQty, decreaseQty } = useCart();
   const router = useRouter();
 
-  const totalPrice = cart.reduce(
+  const totalPrice = c  art.reduce(
     (sum: number, item: any) =>
       sum +
       Number(String(item.price).replace(/[^0-9]/g, "") || 0) *
